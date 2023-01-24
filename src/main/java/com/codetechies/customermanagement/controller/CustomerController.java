aa350ba8c3cc333e78e6dcc61d306eb95197ca5c
amitdatta <the.amisoft@gmail.com>
2023-01-24 10:14:34 +0530

    JPA

--- src/main/java/com/codetechies/customermanagement/controller/CustomerController.java
+++ src/main/java/com/codetechies/customermanagement/controller/CustomerController.java
@@ -3,33 +3,22 @@ package com.codetechies.customermanagement.controller;
 import com.codetechies.customermanagement.dto.CustomerAccountResponse;
 import com.codetechies.customermanagement.dto.CustomerDetails;
 import com.codetechies.customermanagement.service.CustomerService;
-import io.swagger.v3.oas.annotations.Operation;
-import io.swagger.v3.oas.annotations.responses.ApiResponse;
-import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.AllArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @AllArgsConstructor
 @Tag(name = "Customer-Management")
-public class CustomerController {
+public class CustomerController implements CustomerApi{
 
     private CustomerService customerService;
-    @Operation(summary = "This endpoint will register a new customer and creates a new " +
-            "customer records in database")
-    @ApiResponses(value = {@ApiResponse(responseCode = "500", description = "Internal server error"),
-            @ApiResponse(responseCode = "200", description = "Ok"),
-            @ApiResponse(responseCode = "401", description = "Unauthorized access")})
-    @PostMapping("/registerNewCustomer")
-    ResponseEntity<CustomerAccountResponse> createNewAccount(@RequestBody CustomerDetails customerDetails) {
 
+    @Override
+    public ResponseEntity<CustomerAccountResponse> createNewAccount(CustomerDetails customerDetails) {
         var accountResponse = customerService.saveCustomer(customerDetails);
         return ResponseEntity.status(HttpStatus.OK).body(accountResponse);
     }
-
 }
